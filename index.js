c1571475634832e303aca1884d21fc6f9245be93
yakuthemes <info@yakuthemes.com>
2021-07-15 14:04:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+#! /usr/bin/env node
+
+const fs = require('fs')
+const program = require('commander')
+const twig = require('twig').twig
+const glob = require('glob')
+const path = require('path')
+const beautify = require('js-beautify').html
+const rcsCore = require('rcs-core')
+const rcs = require('rename-css-selectors')
+
+program
+  .version('1.0.0')
+  .usage('[directory] --output [directory] --root [directory]')
+  .description('Twig bundler for yakuthemes products.')
+  .option(
+    '-s, --source <directory>',
+    'define the output directory'
+  )
+  .option(
+    '-o, --output <directory>',
+    'define the output directory'
+  )
+  .option(
+    '-r, --root <path>',
+    'define the root path [optional]'
+  )
+  .option(
+    '-d, --data <path>',
+    'define the data path [optional]'
+  )
+  .action(dir => {
+    const option = {
+      ignore: ['node_modules/**']
+    }
+
+    // dir.data.forEach(source => {
+    //   data = JSON.parse(fs.readFileSync(source))
+    //   console.log(data)
+    // })
+
+    // rcsCore.baseLibrary.setExclude(/<%=[\s\S]+%>/);
+
+    // rcs.process.auto(['dist/assets/css/theme.css', 'dist/index.html']);
+
+    glob(dir.source, option, (er, files) => {
+      files.forEach(source => {
+        const file = path.parse(source).base
+        const output = path.normalize(dir.output + file)
+        const dataSource = glob.sync(dir.data, [option])
+
+        const obj = {}
+        let data = ''
+        let parsed = ''
+
+        dataSource.forEach(file => {
+          parsed = JSON.parse(fs.readFileSync(file))
+          data = Object.assign(obj, parsed)
+        })
+
+        twig({
+          path: source,
+          load: template => {
+            const content = template.render(data)
+
+            fs.writeFileSync(output, beautify(content, { extra_liners: ' ', preserve_newlines: false }))
+          }
+        })
+      })
+    })
+  })
+  .parse()
